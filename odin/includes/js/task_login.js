731e560d2f2bf13c347dff3b686d31ce3a8243d4
erinkcreates <erinkcreates@gmail.com>
2022-12-31 11:05:27 -0600

    Complete Check login task - the === (not =) operators were my downfall!

--- odin/includes/js/task_login.js
+++ odin/includes/js/task_login.js
@@ -3,5 +3,15 @@
 let userName = prompt("Username:");
 
 if (userName === "Admin") {
-    let password = prompt("Password:");
+
+    let password = prompt("Password:", '');
+
+    if (password === "a") {
+        alert( "Welcome!" );
+    } else if (password === '' || password === null) {
+        alert( "Cancelled" );
+    } else {
+        alert( "Wrong password" );
+    }
+
 }
